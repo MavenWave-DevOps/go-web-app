59e21f01a21d2abcdf7165605b43ddf63104df90
tony-mw <81250444+tony-mw@users.noreply.github.com>
2022-03-23 11:19:32 -0600

    pretend pipeline

---
+++ pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+PROJECT_ID="prj-cicd-poc-shared-icce"
+VERSION=0.0.1-dev
+
+gcloud config set project $PROJECT_ID
+
+docker build . -t gcr.io/$PROJECT_ID/mw-poc-go-webapp:$VERSION
+docker push gcr.io/$PROJECT_ID/mw-poc-go-webapp:$VERSION
